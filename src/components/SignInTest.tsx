e0615d8823e0c17caddf516063b8d438670e6104
abg3000 <abg3000@naver.com>
2022-02-11 18:58:48 +0900

    [Fix] 존재하지 않는 사용자 예외처리
    - 유저 로그인 예외처리

--- src/components/SignInTest.tsx
+++ src/components/SignInTest.tsx
@@ -3,20 +3,25 @@ import { Dispatch } from 'redux';
 import { connect } from 'react-redux';
 import { RootState } from 'redux/reducer';
 import { signIn, signInAction } from 'redux/actions/userAction';
+import { User } from 'types/user';
 
 interface reduxProps {
+  userList: User[];
   signIn: (name: string) => signInAction;
 }
-function SignInTest({ signIn }: reduxProps) {
+function SignInTest({ userList, signIn }: reduxProps) {
   const [name, setName] = useState<string>('');
 
   const signInHandler = useCallback(
     (e: React.FormEvent) => {
       e.preventDefault();
-      console.log('로그인', name);
-      if (name.length > 0) {
+
+      if (name.length > 0 && userList.some((user) => user.userName === name)) {
+        console.log('로그인', name);
         signIn(name);
         setName('');
+      } else {
+        alert('존재하지 않는 사용자입니다.');
       }
     },
     [name]
@@ -43,7 +48,7 @@ function SignInTest({ signIn }: reduxProps) {
 
 export default connect(
   (state: RootState) => ({
-    user: state.userReducer.isLoggin,
+    userList: state.userReducer.userList,
   }),
   (dispatch: Dispatch) => ({
     signIn: (name: string) => dispatch(signIn(name)),
