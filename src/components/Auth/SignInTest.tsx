b80a82ab6fb6fcb1b869633a7aab3ca61ce2e3b3
abg3000 <abg3000@naver.com>
2022-02-11 22:35:11 +0900

    [Feat] 로그인 스타일링

--- src/components/Auth/SignInTest.tsx
+++ src/components/Auth/SignInTest.tsx
@@ -4,7 +4,7 @@ import { connect } from 'react-redux';
 import { RootState } from 'redux/reducer';
 import { signIn, signInAction } from 'redux/actions/userAction';
 import { User } from 'types/user';
-
+import './style.scss';
 interface reduxProps {
   userList: User[];
   signIn: (name: string) => signInAction;
@@ -15,7 +15,6 @@ function SignInTest({ userList, signIn }: reduxProps) {
   const signInHandler = useCallback(
     (e: React.FormEvent) => {
       e.preventDefault();
-
       if (name.length > 0 && userList.some((user) => user.userName === name)) {
         console.log('로그인', name);
         signIn(name);
@@ -31,18 +30,20 @@ function SignInTest({ userList, signIn }: reduxProps) {
     setName(e.target.value);
   };
   return (
-    <>
-      <p>
-        이름을 적어주세요. (이광수, 김종국, 유재석 으로 로그인 해주세요, 회원가입 기능은 구현하지
-        않았습니다.)
-      </p>
-      <form onSubmit={signInHandler}>
-        <label htmlFor='sign-in'>
-          <input type='text' id='sign-in' onChange={onChangeHandler} value={name} />
-        </label>
-        <button type='submit'>로그인</button>
-      </form>
-    </>
+    <div className='signIn-container'>
+      <div className='signIn-form'>
+        <div className='form-description'>
+          <p>이름을 적어주세요.</p>
+          <p> (이광수, 김종국, 유재석 으로 로그인 해주세요.)</p>
+        </div>
+        <form onSubmit={signInHandler}>
+          <label htmlFor='sign-in'>
+            <input type='text' id='sign-in' onChange={onChangeHandler} value={name} />
+          </label>
+          <button type='submit'>로그인</button>
+        </form>
+      </div>
+    </div>
   );
 }
 
